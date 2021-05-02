a30b4f051e016f7cbd1b6b9dd6678dea51d7eac6
xjyplayer <2532917800@qq.com>
2021-05-02 21:08:16 +0800

    全部完成

--- repository_template_model/user_detail/query.go
+++ repository_template_model/user_detail/query.go
@@ -15,7 +15,7 @@ func GetUserAuthority(uid int64) []UserDetail {
 	mysql.NewBuild(&UserDetails).Select("us_user.id as uid, us_user.username as username, us_user.add_datetime as add_datetime, us_user.upt_datetime as upt_datetime, c.id as role_id, c.role_name as role_name, e.id as authority_id, e.authority_name as authority_name").
 		LeftJoin(ret_user_role.RetUserRole{}.TableName()+" as a", "a.uid = us_user.id").
 		LeftJoin(role.Role{}.TableName()+" as c", "a.role_id = c.id").
-		LeftJoin(ret_role_authority.RetRoleAuthority{}.TableName()+"as d", "c.id = d.role_id").
+		LeftJoin(ret_role_authority.RetRoleAuthority{}.TableName()+" as d", "c.id = d.role_id").
 		LeftJoin(authority.Authority{}.TableName()+" as e", "d.authority_id = e.id").Where(clos.Id, uid).
 		Get()
 
